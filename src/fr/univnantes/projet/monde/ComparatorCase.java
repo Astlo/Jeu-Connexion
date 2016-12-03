63f7fe9cd90f5c7dc20651c88eb795d626059366
Astlo <lebars.yannis@gmail.com>
2016-12-03 15:36:47 +0100

    Add ComparatorCase

---
+++ src/fr/univnantes/projet/monde/ComparatorCase.java
@@ -0,0 +1,15 @@
+package fr.univnantes.projet.monde;
+import java.util.Comparator;
+
+/**
+ * Permet de comparer deux cases
+ * @author LE BARS Yannis & HUNAULT Marion
+ *
+ */
+public class ComparatorCase implements Comparator<Case> {
+	
+	@Override
+	public int compare(Case c1, Case c2) {
+		return Integer.compare(c2.getHauteurMax(), c1.getHauteurMax());
+	}
+}
